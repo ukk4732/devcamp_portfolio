b335a3588f407908ccaaf7840413541388f28c1e
Umesh <ukk4732@gmail.com>
2019-09-19 19:59:40 +0530

    Added default image for portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,12 @@ class Portfolio < ApplicationRecord
   def self.angular
     where(subtitle: "Angular")
   end
+
+  after_initialize :set_default_image
+
+  def set_default_image
+    self.main_image ||= "https://via.placeholder.com/600x400/0000FF/808080%20?Text=Digital.com%20C/O%20https://placeholder.com/"
+    self.thumb_image ||= "https://via.placeholder.com/350x200/0000FF/808080%20?Text=Digital.com%20C/O%20https://placeholder.com/"
+  end
+
 end
