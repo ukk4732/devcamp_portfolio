ed8198494a688533c5511c72094cf8d87a9097da
Umesh <ukk4732@gmail.com>
2019-09-27 18:35:40 +0530

    Added first_name and last_name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Validation
+  validates_presence_of :name
+
+  #Virtual attributes
+  def first_name
+    self.name.present? ? self.name.split.first : "User"
+  end
+
+  def last_name
+    self.name.present? ? self.name.split.last : "User"
+  end
 end
