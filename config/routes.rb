c1b9d6779941ce1739cfc94ef72a82c8d11ea180
Umesh <ukk4732@gmail.com>
2019-09-27 10:51:52 +0530

    Customize devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sogn_out: 'logout', sign_up: 'register' }
+
   root to: 'pages#home'
   
   resources :portfolios, except: [:show]
