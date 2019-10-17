9271f17a53648cd0fe178982ba4e9dd76547cefe
Umesh <ukk4732@gmail.com>
2019-10-17 19:21:49 +0530

    Added dropdown for blogs topic

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.page(params[:page]).per(5)
+    @blogs = Blog.order("created_at DESC").page(params[:page]).per(5)
     @page_header = "DevcampPortfolio | My Blogs"
   end
 
@@ -73,6 +73,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body)
+      params.require(:blog).permit(:title, :body, :topic_id)
     end
 end
